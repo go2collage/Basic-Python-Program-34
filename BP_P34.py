72745a597290949a73177276152bd748ce45de34
go2collage <91822422+go2collage@users.noreply.github.com>
2023-03-12 14:54:16 +0530

    Add files via upload

---
+++ BP_P34.py
@@ -0,0 +1,16 @@
+# find min & max value in a set
+
+# also find length of set
+
+# create set
+
+set1 = set([5, 10, 15, 20, 25, 30, 35, 40, 45, 50])
+
+print("set1 is: ", set1)
+
+print("Maximum value of set1 is: ", max(set1))      # using max() function
+print("Minimum value of set1 is: ", min(set1))      # using min() function
+
+print("Length of set1 is: ", len(set1))             # using len() function
+
+# Thanks for Watching
